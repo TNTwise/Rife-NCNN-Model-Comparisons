f139249dec2dee9b1d07fff7db2d51865b389843
Paxson Gottschalk <tntwise3@gmail.com>
2023-11-16 12:07:25 -0600

    add readme

--- render_all.py
+++ render_all.py
@@ -1,6 +1,10 @@
 import os
 
 for i in os.listdir():
-    os.system(f'./rife-ncnn-vulkan -m {i} -0 0.png -1 2.png -o {i}/1.png')
-    os.system(f'cp *.png {i}/')
-
+    try:
+        os.system(f'./rife-ncnn-vulkan -m {i} -0 0.png -1 2.png -o {i}/1.png')
+        os.system(f'cp *.png {i}/')
+        with open(f'{i}/README.md','w')as f:
+            f.write(f'![{i}](1.png)')
+    except:
+        print("Not a model")
